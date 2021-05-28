388dab6912e9d5d87455ed0ed5e7cf9e596c3aa0
Zoe Talent <zoe.talent@vodafone.com>
2021-05-28 09:04:45 +0100

    Updated line comment to list bikeshare data files required for project

--- bikeshare.py
+++ bikeshare.py
@@ -3,9 +3,11 @@ import calendar
 import pandas as pd
 import numpy as np
 
+# Data files required for project 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
+			  
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
